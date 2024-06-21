18e331c34d88a6ba58ca1862b8f98127c6f07b03
Shiv Bhonde <shivbhonde04@gmail.com>
2024-06-21 12:43:50 +0530

    remove unused isSolidityFramework func

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,12 +1,6 @@
-import { SolidityFramework } from "../types";
-
 export const baseDir = "base";
 
 export const SOLIDITY_FRAMEWORKS = {
   HARDHAT: "hardhat",
   FOUNDRY: "foundry",
 } as const;
-
-export const isSolidityFramework = (value: string): value is SolidityFramework => {
-  return Object.values(SOLIDITY_FRAMEWORKS).includes(value as SolidityFramework);
-};
