fd3b4be6b27976ada59c5ad68dcf47e556b017b8
Shiv Bhonde <shivbhonde04@gmail.com>
2024-06-26 23:05:43 +0530

    clean prettier-format logic

--- src/tasks/prettier-format.ts
+++ src/tasks/prettier-format.ts
@@ -3,47 +3,46 @@ import path from "path";
 import { Options } from "../types";
 import { SOLIDITY_FRAMEWORKS } from "../utils/consts";
 
+async function runPrettier(targetPath: string, prettierConfigPath: string, prettierPlugins: string[]) {
+  const result = await execa("yarn", [
+    "prettier",
+    "--write",
+    targetPath,
+    "--config",
+    prettierConfigPath,
+    ...prettierPlugins,
+  ]);
+  if (result.failed) {
+    throw new Error(`There was a problem running prettier in ${targetPath}`);
+  }
+}
+
 export async function prettierFormat(targetDir: string, options: Options) {
   try {
     const nextJsPath = path.join(targetDir, "packages", "nextjs");
     const nextPrettierConfig = path.join(nextJsPath, ".prettierrc.json");
-    const result = await execa("yarn", [
-      "prettier",
-      "--write",
-      nextJsPath,
-      "--config",
-      nextPrettierConfig,
-      "--plugin=@trivago/prettier-plugin-sort-imports",
-    ]);
-    if (result.failed) {
-      throw new Error("There was a problem running the prettier in nextjs package");
-    }
+
+    await runPrettier(nextJsPath, nextPrettierConfig, ["--plugin=@trivago/prettier-plugin-sort-imports"]);
 
     if (options.extensions.includes(SOLIDITY_FRAMEWORKS.HARDHAT)) {
       const hardhatPackagePath = path.join(targetDir, "packages", SOLIDITY_FRAMEWORKS.HARDHAT);
       const hardhatPrettierConfig = path.join(hardhatPackagePath, ".prettierrc.json");
-      const hardhatResult = await execa("yarn", [
-        "prettier",
-        "--write",
+      const hardhatPaths = [
         `${hardhatPackagePath}/*.ts`,
         `${hardhatPackagePath}/deploy/**/*.ts`,
         `${hardhatPackagePath}/scripts/**/*.ts`,
         `${hardhatPackagePath}/test/**/*.ts`,
         `${hardhatPackagePath}/contracts/**/*.sol`,
-        "--config",
-        hardhatPrettierConfig,
-        "--plugin=prettier-plugin-solidity",
-      ]);
-      if (hardhatResult.failed) {
-        throw new Error("There was a problem running prettier in the hardhat package");
-      }
+      ];
+
+      await runPrettier(hardhatPaths.join(" "), hardhatPrettierConfig, ["--plugin=prettier-plugin-solidity"]);
     }
 
     if (options.extensions.includes(SOLIDITY_FRAMEWORKS.FOUNDRY)) {
       const foundryPackagePath = path.resolve(targetDir, "packages", SOLIDITY_FRAMEWORKS.FOUNDRY);
       const foundryResult = await execa("forge", ["fmt"], { cwd: foundryPackagePath });
       if (foundryResult.failed) {
-        throw new Error("There was a problem running the forge fmt in the foundry package");
+        throw new Error("There was a problem running forge fmt in the foundry package");
       }
     }
   } catch (error) {
