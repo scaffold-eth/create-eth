d39304fa2dc711236ee5ba8a71d017f2f3d383a5
Shiv Bhonde <shivbhonde04@gmail.com>
2024-07-07 13:45:30 +0900

    create output in dist/create-extension

--- rollup.dev.config.js
+++ rollup.dev.config.js
@@ -4,7 +4,7 @@ import autoExternal from "rollup-plugin-auto-external";
 export default {
   input: "src/dev/create-extension.ts",
   output: {
-    dir: "dist/create-extension-cli",
+    dir: "dist/create-extension",
     format: "es",
     sourcemap: true,
   },
