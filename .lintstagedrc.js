447bc1fba9f62f0506ea4b9ea24d7a402dcab896
Rinat <rinat@hey.com>
2024-08-27 18:17:20 +0300

    fix: remove formatting on precommit

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,8 +1,5 @@
-const formatCommand = filenames => `yarn format ${filenames.join(" ")}`;
 const lintCommand = filenames => `yarn lint ${filenames.filter(file => !file.includes("/templates/")).join(" ")}`;
 
 export default {
-  "**/*": [formatCommand],
-
   "**/*.{js,jsx,ts,tsx}": [lintCommand],
 };
