8cb455d13f015d387664a5657fa3d9b261f1426c
Rinat <rinat@hey.com>
2024-08-28 19:45:34 +0300

    disable validate foundry

--- src/cli.ts
+++ src/cli.ts
@@ -5,7 +5,7 @@ import { renderIntroMessage } from "./utils/render-intro-message";
 import type { Args } from "./types";
 import chalk from "chalk";
 import { SOLIDITY_FRAMEWORKS } from "./utils/consts";
-import { validateFoundryUp } from "./utils/system-validation";
+// import { validateFoundryUp } from "./utils/system-validation";
 import { showHelpMessage } from "./utils/show-help-message";
 
 export async function cli(args: Args) {
@@ -19,7 +19,7 @@ export async function cli(args: Args) {
 
     const options = await promptForMissingOptions(rawOptions, solidityFrameworkChoices);
     if (options.solidityFramework === SOLIDITY_FRAMEWORKS.FOUNDRY) {
-      await validateFoundryUp();
+      // await validateFoundryUp();
     }
 
     await createProject(options);
