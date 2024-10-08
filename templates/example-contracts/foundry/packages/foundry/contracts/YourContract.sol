b0611a63761bf1b5d0ed61c980adb76e43aa4414
Shiv Bhonde <shivbhonde04@gmail.com>
2024-10-08 13:29:10 +0700

    format YourContract.sol

--- templates/example-contracts/foundry/packages/foundry/contracts/YourContract.sol
+++ templates/example-contracts/foundry/packages/foundry/contracts/YourContract.sol
@@ -21,7 +21,12 @@ contract YourContract {
   mapping(address => uint256) public userGreetingCounter;
 
   // Events: a way to emit log statements from smart contract that can be listened to by external parties
-  event GreetingChange(address indexed greetingSetter, string newGreeting, bool premium, uint256 value);
+  event GreetingChange(
+    address indexed greetingSetter,
+    string newGreeting,
+    bool premium,
+    uint256 value
+  );
 
   // Constructor: Called once on contract deployment
   // Check packages/foundry/deploy/Deploy.s.sol
@@ -68,12 +73,12 @@ contract YourContract {
    * The function can only be called by the owner of the contract as defined by the isOwner modifier
    */
   function withdraw() public isOwner {
-    (bool success, ) = owner.call{value: address(this).balance}("");
+    (bool success,) = owner.call{ value: address(this).balance }("");
     require(success, "Failed to send Ether");
   }
 
   /**
    * Function that allows the contract to receive ETH
    */
-  receive() external payable {}
+  receive() external payable { }
 }
