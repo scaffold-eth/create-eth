f0e7d6c5bfe19100ad75b2252113ebabb1086024
Damian <damianmarti@gmail.com>
2024-11-20 19:53:33 -0300

    Fix indentation

--- src/curated-extensions.ts
+++ src/curated-extensions.ts
@@ -5,19 +5,21 @@ interface ExternalExtensionWithName extends ExternalExtension {
   name: string;
 }
 
-const CURATED_EXTENSIONS: { [key: string]: ExternalExtension } = extensions.map((extension: ExternalExtensionWithName) => {
-  return {
-    [extension.name]: {
-      repository: extension.repository,
-      branch: extension.branch,
-      description: extension.description,
-      installCommand: extension.installCommand,
-      builder: extension.builder,
-      coBuilders: extension.coBuilders,
-    },
-  };
-}).reduce((acc, extension) => {
-  return { ...acc, ...extension };
-});
+const CURATED_EXTENSIONS: { [key: string]: ExternalExtension } = extensions
+  .map((extension: ExternalExtensionWithName) => {
+    return {
+      [extension.name]: {
+        repository: extension.repository,
+        branch: extension.branch,
+        description: extension.description,
+        installCommand: extension.installCommand,
+        builder: extension.builder,
+        coBuilders: extension.coBuilders,
+      },
+    };
+  })
+  .reduce((acc, extension) => {
+    return { ...acc, ...extension };
+  });
 
 export { CURATED_EXTENSIONS };
