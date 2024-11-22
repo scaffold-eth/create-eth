b6b8b9906d5b053f07bc4bc613b7eab1062f562a
Damian <damianmarti@gmail.com>
2024-11-22 10:42:20 -0300

    Get name from branch on curated-extensions.ts

--- src/curated-extensions.ts
+++ src/curated-extensions.ts
@@ -2,13 +2,20 @@ import { ExternalExtension } from "./types";
 import extensions from "./extensions.json";
 
 interface ExternalExtensionWithName extends ExternalExtension {
-  name: string;
+  name?: string;
 }
 
 const CURATED_EXTENSIONS: { [key: string]: ExternalExtension } = extensions
   .map((extension: ExternalExtensionWithName) => {
+    let name = extension.name;
+    if (!name && extension.branch) {
+      name = extension.branch;
+    }
+    if (!name) {
+      throw new Error("Extension must have a name or branch");
+    }
     return {
-      [extension.name]: {
+      [name]: {
         repository: extension.repository,
         branch: extension.branch,
       },
