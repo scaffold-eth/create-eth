b899ec781965351f12ab9cc281bd7c6f653934c5
Shiv Bhonde <shivbhonde04@gmail.com>
2025-03-12 13:18:07 +0100

    use as const as satisfies while assigning the type

--- templates/base/packages/nextjs/scaffold.config.ts.template.mjs
+++ templates/base/packages/nextjs/scaffold.config.ts.template.mjs
@@ -21,6 +21,8 @@ const contents = ({ preConfigContent, configOverrides, extraConfigTypeName }) =>
 
   return  `import * as chains from "viem/chains";
 
+${preConfigContent[0] || ''}
+
 export type BaseConfig = {
   // The networks on which your DApp is live
   targetNetworks: readonly chains.Chain[];
@@ -47,10 +49,8 @@ export type BaseConfig = {
 export type ScaffoldConfig = BaseConfig ${extraConfigTypeName[0] ? `& ${extraConfigTypeName[0]}` : ''};
 
 export const DEFAULT_ALCHEMY_API_KEY = "oKxs-03sij-U_N0iOlrSsZFr29-IqbuF";
-${preConfigContent[0] || ''}
-;
 
-const scaffoldConfig: ScaffoldConfig = ${stringify(finalConfig)};
+const scaffoldConfig = ${stringify(finalConfig)} as const satisfies ScaffoldConfig;
 
 export default scaffoldConfig;`;
 };
