bf418b22213cc341f1f36d7ffe52e8c57b26ec99
Shiv Bhonde | shivbhonde.eth <shivbhonde04@gmail.com>
2026-01-13 12:39:48 +0530

    add efp extension to org (#378)

--- src/extensions/organizations.ts
+++ src/extensions/organizations.ts
@@ -20,4 +20,11 @@ export const organizations: Extension[] = [
       "This extension helps you get started with SIWE (Sign-In with Ethereum). Authenticate using your Ethereum wallet. No passwords needed.",
     repository: "https://github.com/signinwithethereum/scaffold-siwe-ext",
   },
+  {
+    extensionFlagValue: "ethereumidentitykit/scaffold-efp-ext",
+    name: "EFP Extension",
+    description:
+      "This extension adds Ethereum Follow Protocol (EFP) social graph functionality to your Scaffold-ETH 2 project. EFP is the onchain social graph protocol for Ethereum accounts - like Twitter follows, but fully decentralized and portable across any dApp.",
+    repository: "https://github.com/ethereumidentitykit/scaffold-efp-ext",
+  },
 ];
