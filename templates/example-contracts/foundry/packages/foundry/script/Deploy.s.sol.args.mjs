bb6f2cee6764f04a8df500a127b10f472931c39c
Sebastian Raba <sebastianraba@Sebastians-MacBook-Pro.local>
2026-02-19 15:56:07 +0200

    feat(create-hbar): replace eth references and delete unused modules

--- templates/example-contracts/foundry/packages/foundry/script/Deploy.s.sol.args.mjs
+++
@@ -1,5 +0,0 @@
-export const preContent = `import { DeployYourContract } from "./DeployYourContract.s.sol";`;
-export const deploymentsLogic = `
-    DeployYourContract deployYourContract = new DeployYourContract();
-    deployYourContract.run();
-`;
