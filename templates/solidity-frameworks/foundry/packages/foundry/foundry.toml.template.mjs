efcd0e13362fff544c317373d9931af54099b7e2
Rinat <rinatkins@gmail.com>
2026-04-21 16:14:14 +0400

    fix: verifyAll

--- templates/solidity-frameworks/foundry/packages/foundry/foundry.toml.template.mjs
+++ templates/solidity-frameworks/foundry/packages/foundry/foundry.toml.template.mjs
@@ -11,6 +11,8 @@ src = 'contracts'
 out = 'out'
 libs = ['lib', 'node_modules']
 fs_permissions = [{ access = "read-write", path = "./"}]
+optimizer = true
+optimizer_runs = 200
 ${extraProfileDefaults.filter(Boolean).join("\n")}
 
 [rpc_endpoints]
